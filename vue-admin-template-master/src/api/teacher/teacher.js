9822544a5a9edf1ed6af2dbfffdab24a49507b15
longlang <1733870578@qq.com>
2020-05-07 15:54:02 +0800

    p75      1讲师列表和添加讲师页面      2，先在路由里面index.js添加路由      3teacher.js里面添加接口      4 再添加页面

---
+++ vue-admin-template-master/src/api/teacher/teacher.js
@@ -0,0 +1,28 @@
+import request from '@/utils/request'
+
+//
+// export function getList(params) {
+//
+//   return request({
+//     url:'/table/list',
+//     method:'get',
+//   params
+// })
+// }
+
+//第二种写法
+export  default {
+
+    //讲师列表  条件查询分页
+  //current 当前页 limit每页记录数  teacherquery条件对象
+  getTeacherListPage(current,limit,teacherQuery){
+    return request({
+      //url:'/eduservice/edu-teacher/pageTeacherCondition/'+current+'/'+limit,
+      url:'/eduservice/edu-teacher/pageTeacherCondition/${current}/${limit}',
+      method:'post',
+      //teacherquery条件对象   后端使用RequestBody获取数据
+      //data 表示把对象转换成json传递到接口中去
+      data: teacherQuery
+    })
+  }
+}
