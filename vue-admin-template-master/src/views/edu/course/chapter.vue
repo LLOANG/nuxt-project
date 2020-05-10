0305c2adeafb0786b6839777e3f53de86e29a591
longlang <1733870578@qq.com>
2020-05-10 16:33:52 +0800

    p118  day 8  章节于小节展示 if(this.$route.params && this.$route.params.id)  传来的是个courseId  要写id

--- vue-admin-template-master/src/views/edu/course/chapter.vue
+++ vue-admin-template-master/src/views/edu/course/chapter.vue
@@ -12,6 +12,37 @@
 
     <el-form label-width="120px">
 
+      <el-button type="text">添加章节</el-button>
+      <!-- 章节 -->
+      <ul class="chanpterList">
+        <li
+          v-for="chapter in chapterVideoList"
+          :key="chapter.id">
+          <p>
+            {{ chapter.title }}
+
+            <span class="acts">
+                <el-button type="text">添加课时</el-button>
+                <el-button style="" type="text">编辑</el-button>
+                <el-button type="text">删除</el-button>
+            </span>
+          </p>
+
+          <!-- 视频 -->
+          <ul class="chanpterList videoList">
+            <li
+              v-for="video in chapter.children"
+              :key="video.id">
+              <p>{{ video.title }}
+                <span class="acts">
+                        <el-button type="text">编辑</el-button>
+                        <el-button type="text">删除</el-button>
+                    </span>
+              </p>
+            </li>
+          </ul>
+        </li>
+      </ul>
       <el-form-item>
         <el-button @click="previous">上一步</el-button>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="next">下一步</el-button>
@@ -21,14 +52,21 @@
 </template>
 <!--课程章节-->
 <script>
+  import chapter from '@/api/teacher/chapter'
     export default {
         name: "chaper",
       data(){
         return{
-          saveBtnDisabled:false
+          saveBtnDisabled:false,
+          chapterVideoList:[],
+          courseId:''
         }
       },
       created(){
+          if(this.$route.params && this.$route.params.id){//courseId来自info页面
+            this.courseId=this.$route.params.id;
+            this.getChapterVideo( this.courseId);
+          }
 
       },
       methods:{
@@ -37,6 +75,12 @@
         },
         previous(){
           this.$router.push({path:`/course/info/1`})
+        },
+        //根据课程id查询章节和小节
+        getChapterVideo(courseId){
+          chapter.getAllChapterVideo(courseId).then(res=>{
+this.chapterVideoList=res.data.items;
+          })
         }
       }
 
