1df39388138c61ddfccb59d2ab30c4939e454423
longlang <1733870578@qq.com>
2020-05-10 09:41:39 +0800

    p110   day7  添加课程信息  带上id

---
+++ vue-admin-template-master/src/api/teacher/course.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/edu/course'
+
+export default {
+  saveCourseInfo(courseInfo) {
+    return request({
+      url: `/eduservice/course/addCourse`,
+      method: 'post',
+      data: courseInfo
+    })
+  }
+}
--- vue-admin-template-master/src/views/edu/course/info.vue
+++ vue-admin-template-master/src/views/edu/course/info.vue
@@ -10,29 +10,74 @@
       <el-step title="最终发布"/>
     </el-steps>
 
-    <el-form label-width="120px">
 
-      <el-form-item>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="next">保存并下一步</el-button>
-      </el-form-item>
-    </el-form>
+
+      <el-form label-width="120px">
+
+        <el-form-item label="课程标题">
+          <el-input v-model="courseInfo.title" placeholder=" 示例：机器学习项目课：从基础到搭建项目视频课程。专业名称注意大小写"/>
+        </el-form-item>
+        <el-form-item label="总课时">
+          <el-input-number :min="0" v-model="courseInfo.lessonNum" controls-position="right" placeholder="请填写课程的总课时数"/>
+        </el-form-item>
+
+        <!-- 所属分类 TODO -->
+
+        <!-- 课程讲师 TODO -->
+
+        <el-form-item label="课程简介">
+          <el-input  v-model="courseInfo.description" controls-position="right" placeholder="请填写课程的总课时数"/>
+        </el-form-item>
+
+        <!-- 课程简介 TODO -->
+
+        <!-- 课程封面 TODO -->
+
+        <el-form-item label="课程价格">
+          <el-input-number :min="0" v-model="courseInfo.price" controls-position="right" placeholder="免费课程请设置为0元"/> 元
+        </el-form-item>
+
+        <el-form-item>
+          <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存并下一步</el-button>
+        </el-form-item>
+      </el-form>
+
   </div>
 </template>
 <!--课程信息-->
+
 <script>
+  import course from '@/api/teacher/course'
     export default {
         name: "info",
       data(){
           return{
-            saveBtnDisabled:false
+            saveBtnDisabled:false,
+            courseInfo:{
+            title: '',
+            subjectId: '',
+            teacherId: '',
+            lessonNum: 0,
+            description: '',
+            cover: '',
+            price: 0
+          }
           }
       },
       created(){
 
       },
       methods:{
-        next(){
-          this.$router.push({path:`/course/chapter/1`})
+        saveOrUpdate(){
+          course.saveCourseInfo(this.courseInfo).then(response=>{
+            this.$message({
+              type: 'success',
+              message: '添加成功'
+            })
+            //跳转到第二步
+            this.$router.push({path:`/course/chapter/`+response.data.courseId})
+          })
+
         }
       }
     }
