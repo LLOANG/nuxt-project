92232cd2142345750597451df8fb037f5eaceb85
longlang <1733870578@qq.com>
2020-05-11 15:08:25 +0800

    p135   课程列表功能

--- vue-admin-template-master/src/api/teacher/course.js
+++ vue-admin-template-master/src/api/teacher/course.js
@@ -44,5 +44,17 @@ export default {
       url: `/eduservice/course/publishCourse/`+id,
       method: 'put'
     })
+  },
+  getCourseListPage(current,limit,CourseQuery){
+    return request({
+      //url:'/eduservice/edu-teacher/pageTeacherCondition/'+current+'/'+limit,
+      //url:'/eduservice/teacher/pageTeacherCondition/${current}/${limit}'
+      // ``  不是单引号  不然                          这里不会变色
+      url:`/eduservice/course/getCoursePage/${current}/${limit}`,
+      method:'get',
+      //teacherquery条件对象   后端使用RequestBody获取数据
+      //data 表示把对象转换成json传递到接口中去
+      data: CourseQuery
+    })
   }
 }
--- vue-admin-template-master/src/views/edu/course/list.vue
+++ vue-admin-template-master/src/views/edu/course/list.vue
@@ -1,13 +1,136 @@
 <template>
+  <div class="app-container">
+    <!--查询表单-->
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item>
+        <el-input v-model="courseQuery.name" placeholder="课程名称"/>
+      </el-form-item>
 
+      <el-form-item>
+        <el-select v-model="courseQuery.status" clearable placeholder="课程状态">
+          <el-option :value="Noamal" label="已经发布"/>
+          <el-option :value="Draft" label="未发布"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="讲师id">
+        <el-date-picker
+          v-model="courseQuery.begin"
+          type="datetime"
+          placeholder="选择开始时间"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          default-time="00:00:00"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-date-picker
+          v-model="courseQuery.end"
+          type="datetime"
+          placeholder="选择截止时间"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          default-time="00:00:00"
+        />
+      </el-form-item>
+
+      <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
+      <el-button type="default" @click="resetData()">清空</el-button>
+    </el-form>
+    <!-- 表格 -->
+    <el-table
+      :data="list"
+      border
+      fit
+      highlight-current-row>
+
+      <el-table-column
+        label="序号"
+        width="70"
+        align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="title" label="课程名称" width="80" />
+
+      <el-table-column label="课程状态" width="80">
+        <template slot-scope="scope">   <!--scope代表整个表格-->
+          {{ scope.row.status==='Draft'?'未发布':'已发布' }}
+        </template><!--   == 判断值  ===判断类型和值 -->
+      </el-table-column>
+
+      <el-table-column prop="lessonNum" label="课时数" />
+
+      <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
+
+      <el-table-column prop="viewCount" label="浏览数量" width="60" />
+
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/teacher/edit/'+scope.row.id"><!--跳转路由-->
+            <el-button type="primary" size="mini" icon="el-icon-edit" >编辑课程基本信息</el-button>
+          </router-link>
+
+          <router-link :to="'/teacher/edit/'+scope.row.id"><!--跳转路由-->
+            <el-button type="primary" size="mini" icon="el-icon-edit" >编辑课程大纲</el-button>
+          </router-link>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+<!--    &lt;!&ndash; 分页 &ndash;&gt;
+    <el-pagination
+      :current-page="page"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, prev, pager, next, jumper"
+      @current-change="getList"
+    />-->
+  </div>
 </template>
-<!--课程列表-->
+
 <script>
-    export default {
-        name: "list"
+
+  //引入teacher.js
+  import course from '@/api/teacher/course'
+
+  export default {
+    data(){//定义变量和初始值
+      return{
+        list:null,//查询之后接口返回集合
+        page:1,
+        limit:10,
+        total:0,
+        courseQuery:{}
+      }
+    },
+    created(){//页面渲染之前执行  一般调用methods定义的方法
+      this.getList();
+    },
+    methods:{//创建具体的方法，调用teacher.js定义的方法
+      //讲师列表方法
+      getList(page=1){//分页点击时需要的当前页数  默认是1   每次点击都是不同的值
+        this.page=page
+        course.getCourseListPage(this.page,this.limit,this.courseQuery).then(response=>{
+          //请求成功
+          //response是借口返回的数据
+          // console.log(response)
+          this.list=response.data.list
+          this.total=response.data.total
+        }).catch(error=>{//请求失败
+          console.log(error)
+        })
+      },
+      resetData() {
+        this.courseQuery = {}
+        this.getList()
+      }
     }
+  }
 </script>
 
 <style scoped>
 
 </style>
+
