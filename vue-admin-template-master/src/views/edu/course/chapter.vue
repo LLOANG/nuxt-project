932f738f123b15040a363a98e3135144b6c31369
longlang <1733870578@qq.com>
2020-05-12 12:27:34 +0800

    p148  147颠倒了   day9
    149
    通过上传视频到云端
      :action="BASE_API+'/eduvod/video/uploadAliyunVideo'"
      然后会返回视频id   videoId
      把这个videoId存到数据库
    
     通过nginx转发，nginx进行配置vod模块的端口  以及对上传视频容量的限制
    笔记 9-3-2

--- vue-admin-template-master/src/views/edu/course/chapter.vue
+++ vue-admin-template-master/src/views/edu/course/chapter.vue
@@ -60,7 +60,25 @@
                 </el-radio-group>
               </el-form-item>
               <el-form-item label="上传视频">
-                <!-- TODO -->
+                <el-upload
+                  :on-success="handleVodUploadSuccess"
+                  :on-remove="handleVodRemove"
+                  :before-remove="beforeVodRemove"
+                  :on-exceed="handleUploadExceed"
+                  :file-list="fileList"
+                  :action="BASE_API+'/eduvod/video/uploadAliyunVideo'"
+                  :limit="1"
+                  class="upload-demo">
+                  <el-button size="small" type="primary">上传视频</el-button>
+                  <el-tooltip placement="right-end">
+                    <div slot="content">最大支持1G，<br>
+                      支持3GP、ASF、AVI、DAT、DV、FLV、F4V、<br>
+                      GIF、M2T、M4V、MJ2、MJPEG、MKV、MOV、MP4、<br>
+                      MPE、MPG、MPEG、MTS、OGG、QT、RM、RMVB、<br>
+                      SWF、TS、VOB、WMV、WEBM 等视频格式上传</div>
+                    <i class="el-icon-question"/>
+                  </el-tooltip>
+                </el-upload>
               </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
@@ -112,8 +130,11 @@
             title:'',
             sort:0,
             free: 0,
-            videoSourceId: ''
-          }
+            videoSourceId: '',
+            videoOriginalName:''//视频名称
+          },
+          fileList: [],//上传文件列表
+          BASE_API: process.env.BASE_API // 接口API地址
         }
       },
       created(){
@@ -124,10 +145,19 @@
 
       },
       methods:{
-
-          //==================================小节操作=======================================================
+        //上传视频成功调用的方法
+        handleVodUploadSuccess(response, file, fileList) {
+          this.video.videoSourceId = response.data.videoId;
+//上传视频名称赋值
+          this.video.videoOriginalName = file.name
+        },
+//视图上传多于一个视频
+        handleUploadExceed(files, fileList) {
+          this.$message.warning('想要重新上传视频，请先删除已上传的视频')
+        },
+        //==================================小节操作=======================================================
         //删除小节
-        removeVideo(videoId){
+        removeVideo(videoId) {
           this.$confirm('此操作将永久删除该记录, 是否继续?', '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
